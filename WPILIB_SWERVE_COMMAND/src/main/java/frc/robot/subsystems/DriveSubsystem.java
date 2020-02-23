5a36196c164b0e385eb91a8db0ede7d1e4679acb
daniel <rdanielbuckley@gmail.com>
2020-02-23 13:19:26 -0500

    tweaked use of pidcontroller

--- WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -84,6 +84,8 @@ public class DriveSubsystem extends SubsystemBase {
     this.rightFrontAbsEncoder = rightFrontAbsEncoder;
     this.leftRearAbsEncoder = leftRearAbsEncoder;
     this.rightRearAbsEncoder = rightRearAbsEncoder;
+    headingController.setTolerance(DriveConstants.kTurnToleranceDeg, DriveConstants.kTurnRateToleranceDegPerS);
+    headingController.enableContinuousInput(-180, 180);
     mCSVWriter = new ReflectingCSVWriter<>(this.getName(), DebugOutput.class);
   }
 
@@ -109,10 +111,8 @@ public class DriveSubsystem extends SubsystemBase {
   public void periodic() {
     resumeCSVWriter();
 
-    headingController.setTolerance(DriveConstants.kTurnToleranceDeg, DriveConstants.kTurnRateToleranceDegPerS);
-    headingController.enableContinuousInput(-180, 180);
     SmartDashboard.putNumber("headingController In", getHeading());
-    headingControllerOutput = headingController.calculate(MathUtil.clamp(getHeading(), -180, 180));
+    //headingControllerOutput = headingController.calculate(MathUtil.clamp(getHeading(), -180, 180));
     SmartDashboard.putNumber("headingController Out", headingControllerOutput);
 
     // Update the odometry in the periodic block
@@ -192,8 +192,7 @@ public class DriveSubsystem extends SubsystemBase {
 
     //If the stick is released, don't change the rotation
     if(stickControlledHeading && (Math.abs(rightX) > DriveConstants.kMinRightStickThreshold || Math.abs(rightY) > DriveConstants.kMinRightStickThreshold)){
-      double rot = getStickAngle(rightX, rightY);
-      headingController.setGoal(rot);
+      rotationalOutput = headingController.calculate(getHeading(), getStickAngle(rightX, rightY));
     } else if(stickControlledHeading && Math.abs(rightX) <= DriveConstants.kMinRightStickThreshold && Math.abs(rightY) <= DriveConstants.kMinRightStickThreshold) {
       rotationalOutput = 0;
     }
