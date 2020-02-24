d41e7eb150d0fbabf5dcee0e1c54c37f3b624f03
Stephen Hamilton <stphnhmltn26@gmail.com>
2020-02-23 20:54:12 -0500

    Repeatable auto

--- WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/autonomous/L1_EnemyPair_Front3.java
+++ WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/autonomous/L1_EnemyPair_Front3.java
@@ -32,12 +32,27 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
     public L1_EnemyPair_Front3(LedStringSubsystem m_ledstring, DriveSubsystem m_robotDrive, IntakeSubsystem m_intake, SequencerSubsystem m_sequence,
             ShootClimbSubsystem m_shootclimb, JevoisVisionSubsystem m_vision, TargetingSubsystem m_targeting) {
                 
-        TrajectoryConfig config =
+        TrajectoryConfig configBackward =
             new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
                                  AutoConstants.kMaxAccelerationMetersPerSecondSquared)
                 // Add kinematics to ensure max speed is actually obeyed
                 .setKinematics(DriveConstants.kDriveKinematics)
                 .setReversed(true);
+
+        TrajectoryConfig configForward =
+            new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+                                 AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                // Add kinematics to ensure max speed is actually obeyed
+                .setKinematics(DriveConstants.kDriveKinematics)
+                .setReversed(false);
+
+                /*
+        TrajectoryConfig configForward = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+                                 AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                // Add kinematics to ensure max speed is actually obeyed
+                .setKinematics(DriveConstants.kDriveKinematics)
+                .setReversed(false);
+                */
     
         // BACKWARD TO ENEMY PAIR
         Trajectory backwardToEnemyPair = TrajectoryGenerator.generateTrajectory(
@@ -51,7 +66,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
             ),
 
             new Pose2d(-2.2, -0.2, new Rotation2d(0)),
-            config
+            configBackward
         );
 
         backwardToEnemyPair = new Trajectory(unrotateTrajectory(backwardToEnemyPair.getStates(), 0)); // make it pure strafe
@@ -73,18 +88,19 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
         // STRAFE TO SHOOT LOCATION
         Trajectory strafeToShootLocation1_1 = TrajectoryGenerator.generateTrajectory(
             // Start at the origin facing the +X direction
-            new Pose2d(-2.2, -0.2, new Rotation2d(0)),
+            new Pose2d(-2.2, -0.2, new Rotation2d(-50)),
 
             //  STRAFE TO SHOOT LOCATION
             List.of(
-                new Translation2d(-1, -0.1)
+                //new Translation2d(-1, -0.1),
+                //new Translation2d(-2, -2.43)
             ),
 
-            new Pose2d(0, -0.32, new Rotation2d(Math.toRadians(0))),
-            config
+            new Pose2d(-0.54, -4.74, new Rotation2d(Math.toRadians(-50))),
+            configForward
         );
     
-        strafeToShootLocation1_1 = new Trajectory(unrotateTrajectory(strafeToShootLocation1_1.getStates(), 0)); // make it pure strafe
+        strafeToShootLocation1_1 = new Trajectory(unrotateTrajectory(strafeToShootLocation1_1.getStates(), -12)); // make it pure strafe
         Utils.printTrajectory(this.getClass().getSimpleName() + ": strafeToShootLocation", strafeToShootLocation1_1);
 
         SwerveControllerCommand strafeToShootLocation1_1Command = new SwerveControllerCommand(
@@ -110,7 +126,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
             ),
 
             new Pose2d(-0.54, -4.74, new Rotation2d(Math.toRadians(-12))),
-            config
+            configBackward
         );
     
         strafeToShootLocation1_1 = new Trajectory(unrotateTrajectory(strafeToShootLocation1_2.getStates(), -12)); // make it pure strafe
@@ -140,7 +156,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
 
             ),
             new Pose2d(-2.14, -3.4, new Rotation2d(Math.toRadians(65))),
-            config
+            configBackward
         );
     
         backwardToPickupFront3_1 = new Trajectory(unrotateTrajectory(backwardToPickupFront3_1.getStates(), 65)); // make it pure strafe
@@ -169,7 +185,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
             ),
 
             new Pose2d(-1.79, -4.08, new Rotation2d(Math.toRadians(70))),
-            config
+            configBackward
         );
     
         backwardToPickupFront3_2 = new Trajectory(maintainTrajectory(backwardToPickupFront3_2.getStates(), 65)); // make it pure strafe
@@ -201,7 +217,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
                 ),
 
                 new Pose2d(-0.59, -4.92, new Rotation2d(-7)),
-                config
+                configBackward
             );
 
             //#endregion
@@ -232,22 +248,28 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
 
             // ENEMY PAIR
             new ParallelCommandGroup(
-                backwardToEnemyPairCommand,
-                new IntakeSeqCommand(m_intake, m_sequence).withTimeout(4)
+                backwardToEnemyPairCommand
+                //new IntakeSeqCommand(m_intake, m_sequence).withTimeout(4)
             ),
 
-            strafeToShootLocation1_1Command,
+            new ParallelCommandGroup(
+                new InstantCommand(m_shootclimb::enableShooting, m_shootclimb).withTimeout(3),
+                strafeToShootLocation1_1Command
+            ),
 
+            /*
             new ParallelCommandGroup(
                 new InstantCommand(m_shootclimb::enableShooting, m_shootclimb).withTimeout(3),
                 strafeToShootLocation1_2Command
             ),
+            */
 
             //new Aim(m_robotDrive, m_vision, m_targeting)),
             new ShootSeqCommandAuto(m_ledstring, m_shootclimb, m_sequence).withTimeout(1),
 
-            new WaitCommand(getSecondaryDelaySeconds()),
+            new WaitCommand(getSecondaryDelaySeconds())
 
+            /*
             // FRONT 3
             backwardToPickupFront3_1Command,
 
@@ -262,7 +284,7 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
             new InstantCommand(m_shootclimb::enableShooting, m_shootclimb).withTimeout(3),
             //new Aim(m_robotDrive, m_vision, m_targeting)),
             new ShootSeqCommandAuto(m_ledstring, m_shootclimb, m_sequence).withTimeout(3)
-
+*/
         );
         //#endregion
 
