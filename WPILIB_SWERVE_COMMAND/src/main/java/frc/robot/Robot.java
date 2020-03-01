ab88ae7d27d68e25e5bc9e3953c55ace61cc0619
daniel <rdanielbuckley@gmail.com>
2020-02-29 21:26:18 -0500

    unintegrate led light string

--- WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/Robot.java
+++ WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends TimedRobot {
     m_sequencer.stop();
     m_shootclimb.stopShooting();
     //m_colorwheel.init();
-    m_ledstring.init();
+    //m_ledstring.init();
   }
 
   /**
@@ -68,7 +68,7 @@ public class Robot extends TimedRobot {
     CameraServer camServer = CameraServer.getInstance();
     camServer.startAutomaticCapture();
     
-    m_ledstring = new LedStringSubsystem();
+    m_ledstring = null; //new LedStringSubsystem();
     m_robotDrive = new DriveSubsystem();
     m_vision = new JevoisVisionSubsystem(m_robotDrive);
     m_intake = new IntakeSubsystem(m_ledstring);
@@ -138,7 +138,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
-    m_ledstring.option(LedOption.TEAM);
+    //m_ledstring.option(LedOption.TEAM);
     m_robotDrive.resetEncoders();
     SmartDashboard.putBoolean("LowSensor",  m_sequencer.lowSensorHasBall());
     SmartDashboard.putBoolean("MidSensor",  m_sequencer.midSensorHasBall());
@@ -153,7 +153,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     CommandScheduler.getInstance().cancelAll(); ///????????????????????????????????????????? SHOULD WE DO THIS????? ******************
     
-    m_ledstring.option(LedOption.RAINBOW);
+    //m_ledstring.option(LedOption.RAINBOW);
 
     //m_robotDrive.resumeCSVWriter();
     m_sequencer.setPowerCellCount((int) SmartDashboard.getNumber("INIT CELL COUNT", 3));
@@ -230,13 +230,13 @@ public class Robot extends TimedRobot {
     SmartDashboard.putString("Intake State",  m_intake.getIntakeState());
     //SmartDashboard.putNumber("Climb Encoder", m_shootclimb.getClimbEncoderValue());
 
-    switch((int)m_sequencer.getPowerCellCount()){
-      case 1: m_ledstring.option(LedOption.BALLONE); break;
-      case 2: m_ledstring.option(LedOption.BALLTWO); break;
-      case 3: m_ledstring.option(LedOption.BALLTHREE); break;
-      case 4: m_ledstring.option(LedOption.BALLFOUR); break;
-      case 5: m_ledstring.option(LedOption.GREEN); break;
-    }
+    // switch((int)m_sequencer.getPowerCellCount()){
+    //   case 1: m_ledstring.option(LedOption.BALLONE); break;
+    //   case 2: m_ledstring.option(LedOption.BALLTWO); break;
+    //   case 3: m_ledstring.option(LedOption.BALLTHREE); break;
+    //   case 4: m_ledstring.option(LedOption.BALLFOUR); break;
+    //   case 5: m_ledstring.option(LedOption.GREEN); break;
+    // }
   }
 
   @Override
