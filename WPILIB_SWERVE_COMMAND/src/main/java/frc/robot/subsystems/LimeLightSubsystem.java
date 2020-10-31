3a81f7a38a30a5cb46e26ebe0e5fe419880d6db3
Stephen Hamilton <stphnhmltn26@gmail.com>
2020-10-31 15:37:37 -0400

    Made a subsystem for the limelight

--- WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ WPILIB_SWERVE_COMMAND/src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -1,19 +1,35 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.vision.GoalTracker;
+import frc.robot.vision.JevoisVisionServer;
+import frc.robot.vision.JevoisVisionUpdate;
+import frc.robot.vision.LimeTargetInfo;
+import frc.robot.vision.ShooterAimingParameters;
+import frc.robot.vision.TargetInfo;
+import frc.robot.vision.GoalTracker.TrackReport;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Transform2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LimeLightSubsystem extends SubsystemBase {
-
-    NetworkTable limeTable;
-    NetworkTableEntry tx;
-    NetworkTableEntry ty;
-    NetworkTableEntry ta;
-
-    /*
+/* Example code for limelight
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+    import edu.wpi.first.networktables.NetworkTable;
+    import edu.wpi.first.networktables.NetworkTableEntry;
+    import edu.wpi.first.networktables.NetworkTableInstance;
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry tx = table.getEntry("tx");
@@ -29,30 +45,55 @@ public class LimeLightSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
+*/
 
-    */
+/**
+ * This function adds vision updates (from the Nexus smartphone) to a list in RobotState. This helps keep track of goals
+ * detected by the vision system. The code to determine the best goal to shoot at and prune old Goal tracks is in
+ * GoalTracker.java
+ * 
+ * @see GoalTracker.java
+ */
+public class LimeLightSubsystem extends SubsystemBase {
 
+    private NetworkTable limeTable;
+    private NetworkTableEntry limeTX;
+    private NetworkTableEntry limeTY;
+    private NetworkTableEntry limeValidTargets;
+    private NetworkTableEntry limeLED;
 
-    public LimeLightSubsystem(){
+    private LimeTargetInfo lastTarget;
+
+    public LimeLightSubsystem() {
         limeTable = NetworkTableInstance.getDefault().getTable("limelight");
-        tx = limeTable.getEntry("tx");
-        ty = limeTable.getEntry("ty");
-        ta = limeTable.getEntry("ta");
+        limeTX = limeTable.getEntry("tx");
+        limeTY = limeTable.getEntry("ty");
+        limeValidTargets = limeTable.getEntry("tv");
+        limeLED = limeTable.getEntry("ledMode");
 
-        SmartDashboard.putNumber("LimeX", 0);
-        SmartDashboard.putNumber("LimeY", 0);
-        SmartDashboard.putNumber("LimeArea", 0);
+        disableLED();
     }
 
     @Override
     public void periodic() {
-        double x = tx.getDouble(0.0);
-        double y = ty.getDouble(0.0);
-        double area = ta.getDouble(0.0);
-
-        SmartDashboard.putNumber("LimeX", x);
-        SmartDashboard.putNumber("LimeY", y);
-        SmartDashboard.putNumber("LimeArea", area);
+        if(hasTarget()){
+            lastTarget = new LimeTargetInfo(limeTX.getDouble(0), limeTY.getDouble(0), Timer.getFPGATimestamp());
+        }
     }
 
+    public LimeTargetInfo getLastTarget(){
+        return lastTarget;
+    }
+
+    public boolean hasTarget(){
+        return limeValidTargets.getDouble(0) > 0;
+    }
+
+    public void enableLED(){
+        limeLED.setNumber(3);
+    }
+
+    public void disableLED(){
+        limeLED.setNumber(1);
+    }
 }
